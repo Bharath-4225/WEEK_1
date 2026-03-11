a166a65133c518debb0e41863b1e9d4967874f0e
Bharath-4225 <bt7407@srmist.edu.in>
2026-03-11 15:53:15 +0530

    Update Practiceq2.java

--- Practiceq2.java
+++ Practiceq2.java
@@ -1,8 +1,11 @@
 public class Practiceq2 {
-    public static void main(String[]args){
-        int dob=2000;
-        int present=2024;
-        System.out.println("Hary's age in "+present+" is "+(present-dob));
+     public static void main(String[]args){
+        float math,phy,chem;
+        math=94;
+        phy=95;
+        chem=96;
+        System.out.println("Sam's average mark in PCM is "+((math+phy+chem)/3));
 
-    } 
+
+    }
 }
