718bfd60309785b288f1d34f4992afab941e7473
Bharath-4225 <bt7407@srmist.edu.in>
2026-03-11 15:50:01 +0530

    level1

---
+++ Practiceq2.java
@@ -0,0 +1,8 @@
+public class Practiceq2 {
+    public static void main(String[]args){
+        int dob=2000;
+        int present=2024;
+        System.out.println("Hary's age in "+present+" is "+(present-dob));
+
+    } 
+}
