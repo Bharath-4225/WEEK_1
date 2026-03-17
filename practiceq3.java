a0ade0cd254c6e96af59d80ea5709104a19c2146
Bharath-4225 <bt7407@srmist.edu.in>
2026-03-17 13:06:33 +0530

    level1

---
+++ practiceq3.java
@@ -0,0 +1,9 @@
+public class practiceq3 
+     {
+    public static void main(String[]args){
+        float dist=10.8f;
+        float mile =(dist*1.6f);
+        System.out.format("The distance 10.8 km in miles is %.2f",mile);
+    }
+    
+}
